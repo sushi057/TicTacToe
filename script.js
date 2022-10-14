529c2567ae77d4251bb46b48511ed6aa8885a001
sushi057 <champsuvash@gmail.com>
2022-10-14 22:04:06 +0545

    Double Click fixed

--- script.js
+++ script.js
@@ -9,14 +9,19 @@ const gameBoard = (() => {
     restartButton.addEventListener('click', () => {
         boardSection.forEach((section) => {
             section.textContent = "";
-    })})
+        })
+    })
 
     boardSection.forEach((section) => {
         section.addEventListener('click', () => {
             section.textContent = game.activePlayer.marker;
+            section.style.pointerEvents = "none";
             game.currentPlayer();
+            game.checkWinner();
         })
     })
+
+    return boardSection;
 })();
 
 
@@ -24,6 +29,7 @@ const game = (() => {
     const playerOne = playerFactor('Player 1', 'X');
     const playerTwo = playerFactor('Player 2', 'O');
     const playerTurn = document.querySelector('#current-player');
+    const boardSection = document.querySelectorAll('.board-section');
 
     let activePlayer = playerOne;
 
@@ -38,9 +44,30 @@ const game = (() => {
         }
     }
 
+    const winningPattern = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    function checkWinner() {
+        winningPattern.forEach((item) => {
+            if (boardSection[item[0]] == this.activePlayer.marker && boardSection[item[1]] == this.activePlayer.marker && boardSection[item[2]] == this.activePlayer.marker){
+                    console.log('randi ko ban');
+                }
+           })
+    }
+
     return {
         activePlayer,
-        currentPlayer
+        currentPlayer,
+        winningPattern,
+        checkWinner
     }
 })();
 
