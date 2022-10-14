694103a9c39d2c23146b9ff3e7b066a3b2ddd356
sushi057 <champsuvash@gmail.com>
2022-10-14 14:00:48 +0545

    Restart button

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ const gameBoard = (() => {
     const boardSection = document.querySelectorAll('.board-section');
     const restartButton = document.querySelector('#restart');
 
-    // restartButton.addEventListener('click', () => {
-    //     boardSection.forEach((section) => {
-    //         section.textContent = "";
-    // })})
+    restartButton.addEventListener('click', () => {
+        boardSection.forEach((section) => {
+            section.textContent = "";
+    })})
 
     boardSection.forEach((section) => {
         section.addEventListener('click', () => {
@@ -21,29 +21,28 @@ const gameBoard = (() => {
     })
 })();
 
-const game = (() => {
-    const xSign = document.querySelector('#x-sign');
-    const oSign = document.querySelector('#o-sign');
-   
-    xSign.addEventListener('click', () => {
-        xSign.classList.add('selected');
-        player1.sign = 'X';
-        player2.sign = 'O';
-        console.log(player1.sign);
-        return player1.sign;
-    })
 
-    oSign.addEventListener('click', () => {
-        oSign.classList.add('selected');
-        player1.sign = 'O';
-        player2.sign = 'X';
-        console.log(player1.sign);
-        return player1.sign;
-    })    
+const game = (() => {
+    // const xSign = document.querySelector('#x-sign');
+    // const oSign = document.querySelector('#o-sign');
+   
+    // xSign.addEventListener('click', () => {
+    //     xSign.classList.add('selected');
+    //     player1.sign = 'X';
+    //     player2.sign = 'O';
+    //     console.log(player1.sign);
+    //     return player1.sign;
+    // })
+
+    // oSign.addEventListener('click', () => {
+    //     oSign.classList.add('selected');
+    //     player1.sign = 'O';
+    //     player2.sign = 'X';
+    //     console.log(player1.sign);
+    //     return player1.sign;
+    // })    
 
 
 })();
 
 
-
-//
